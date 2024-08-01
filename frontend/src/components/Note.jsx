bae00313c53ddddc68667c9fa4d42a27632d800c
dat nguyen <trucdat@gmail.com>
2024-08-01 10:18:25 -0700

    first commit

---
+++ frontend/src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../styles/Note.css"
+
+export default function Note({note, onDelete}) {
+  const formatetedDate = new Date(note.created_at).toLocaleDateString('en-US')
+  return (
+    <div className='note-container'>
+      <p className='note-title'>{note.title}</p>
+      <p className='note-content'>{note.content}</p>
+      <p className='note-date'>{formatetedDate}</p>
+      <button className='delete-button' onClick={() => onDelete(note.id)}>Delete</button>
+    </div>
+  )
+}
---
+++ frontend/src/pages/LoadingIndicator.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../styles/LoadingIndicator.css"
+
+export default function LoadingIndicator() {
+  return (
+    <div className='loading-container'>
+      <div className='loader'></div>
+    </div>
+  )
+}
